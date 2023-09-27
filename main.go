8dada0cd00a1f80428355eba74249c66834b2b3e
Christophe Fergeau <cfergeau@redhat.com>
2023-09-27 15:16:48 +0200

    Don't hardcode gvproxy-windows.exe

--- main.go
+++ main.go
@@ -3,39 +3,33 @@ package main
 import (
 	"os"
 	"os/exec"
-	"path/filepath"
 	"syscall"
 )
 
-func argv() []string {
-	if len(os.Args) > 0 {
-		return os.Args[1:]
+func binaryAndArgs() (string, []string) {
+	// os.Args[0] is 'win32-background-launcher.exe' and needs to be dropped
+	// os.Args[1] is the name of the binary we want to start in the background
+	// os.Args[2:] are the arguments of our binary
+	switch {
+	case len(os.Args) == 0:
+		return "", []string{}
+	case len(os.Args) == 1:
+		return os.Args[1], []string{}
+	default:
+		return os.Args[1], os.Args[2:]
 	}
-	return []string{}
-}
-
-func absolutePath(path string) (string, error) {
-	absPath, err := exec.LookPath(path)
-	if err != nil {
-		return "", err
-	}
-	absPath, err = filepath.Abs(absPath)
-	if err != nil {
-		return "", err
-	}
-	return absPath, nil
 }
 
 func main() {
-	binaryPath, err := absolutePath("./gvproxy-windows.exe")
-	if err != nil {
-		panic(err.Error())
+	binaryPath, args := binaryAndArgs()
+	if binaryPath == "" {
+		panic("missing binary name")
 	}
-	cmd := exec.Command(binaryPath, argv()...)
+	cmd := exec.Command(binaryPath, args...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 0x08000000}
 	cmd.Stdout = os.Stdout
 	cmd.Stdout = os.Stderr
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		panic(err.Error())
 	}
